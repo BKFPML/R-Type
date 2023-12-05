e7dcd4e0e7e3e3515ca7a08dfff042dc6de7d140
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-12-05 11:01:30 +0100

    feat(vcpkg): Use vcpkg as package manager

--- build.sh
+++ build.sh
@@ -3,12 +3,39 @@
 SERVER_NAME="r-type_server"
 CLIENT_NAME="r-type_client"
 
+# mkdir -p build
+# cd build || exit
+
+# cmake ..
+
+# make
+
+# mv ./server/${SERVER_NAME} ../
+# mv ./client/${CLIENT_NAME} ../
+
+# vcpkg
+if [ ! -d "vcpkg" ]; then
+    git clone https://github.com/microsoft/vcpkg.git
+else
+    cd vcpkg
+    git pull origin master
+    cd ..
+fi
+
+# Bootstrap vcpkg
+cd vcpkg
+./bootstrap-vcpkg.sh
+
+# Install dependencies
+./vcpkg install sfml boost-asio
+
+cd ..
+
 mkdir -p build
 cd build || exit
 
-cmake ..
-
-make
+cmake -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake ..
+cmake --build .
 
 mv ./server/${SERVER_NAME} ../
 mv ./client/${CLIENT_NAME} ../
