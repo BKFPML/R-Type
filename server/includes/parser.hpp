18a3209893d422d23ea0b939618c34e96cc7a377
Felixsta123 <felixstavonhagen@gmail.com>
2023-12-26 12:22:29 +0100

    add/parser: simple parsing using unordered map for efficiency, with main for testing parser

---
+++ server/includes/parser.hpp
@@ -0,0 +1,52 @@
+/**
+ * @file parser.hpp
+ * @brief 
+ * @date 26-12-2023
+ */
+
+// message example:x:0-y:10-hp:40-ip:127.0.0.1:13151
+
+#pragma once
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map>
+
+/**
+ * @brief Parser class
+ * This class is used to parse the message received from the client
+ * 
+ */
+class Parser {
+    public:
+
+        /**
+         * @brief Construct a new Parser object
+         * 
+         */
+        Parser();
+
+        /**
+         * @brief Main function of the parser
+         * 
+         */
+        std::unordered_map<std::string, std::string> parseMessage(const std::string& message);
+
+    private:
+        /**
+         * @brief Extract the key from a pair
+         * 
+         * @param pair 
+         * @return std::string 
+         */
+        std::string extractKey(const std::string& pair);
+
+        /**
+         * @brief Extract the value from a pair
+         * 
+         * @param pair 
+         * @return std::string 
+         */
+        std::string extractValue(const std::string& pair);
+};
