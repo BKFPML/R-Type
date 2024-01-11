af91a1fc6c53ae1628a71ad5bdda3a5432cd37ef
Ben Desprets <benjy.desprets@gmail.com>
2024-01-11 18:44:47 +0100

    add(doc): documentation for drawEntities

--- client/src/rendering.cpp
+++ client/src/rendering.cpp
@@ -7,13 +7,16 @@
 
 #include "../includes/client.hpp"
 
+/**
+ * @brief Draws all drawable entities
+ *
+ */
 void rtype::Client::drawEntities()
 {
     std::vector<ECS::Entity> entities = _ecs.getDrawableEntities();
     for (auto& entity : entities) {
         Position *pos = _ecs.getComponent<Position>(entity);
         Sprite *sprite = _ecs.getComponent<Sprite>(entity);
-        _graphical->drawSprite(sprite->texture, pos->x, pos->y, 3, 180, 34, 34);
+        _graphical->drawSprite(sprite->texture, pos->x, pos->y, 3, 180, 34, 34); // TODO: derive the sprite size from the texture
     }
 }
-
