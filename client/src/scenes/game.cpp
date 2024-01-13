811c60ccbd083b3ff93727546db5b6e1cb42f591
Paul THIEBAULT <paul.thiebault@epitech.eu>
2024-01-13 19:40:16 +0100

    add(client): Sky Parallax with sinusoidal effect

--- client/src/scenes/game.cpp
+++ client/src/scenes/game.cpp
@@ -15,12 +15,12 @@ void rtype::Client::drawGame()
     _graphical->clear();
     drawParallax();
     drawEntities();
+    drawSky();
     _graphical->display();
 }
 
 /**
  * @brief Draws the background Parallax (three layers)
- *
  */
 void rtype::Client::drawParallax()
 {
@@ -50,4 +50,26 @@ void rtype::Client::drawParallax()
     _graphical->drawSprite("parallax80", _parallaxPos.at(3).first, _parallaxPos.at(3).second, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     _graphical->drawSprite("parallax60", _parallaxPos.at(4).first, _parallaxPos.at(4).second, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     _graphical->drawSprite("parallax60", _parallaxPos.at(5).first, _parallaxPos.at(5).second, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+}
+
+/**
+ * @brief Draw the Sky Parallax with a sinusoidal movement
+ */
+void rtype::Client::drawSky()
+{
+    static float movement = 0.0f;
+    movement += 0.03f;
+
+    _skyPos.at(0).first -= 5;
+    _skyPos.at(1).first -= 5;
+
+    if (_skyPos.at(0).first <= -WINDOW_WIDTH)
+        _skyPos.at(0).first = WINDOW_WIDTH;
+    if (_skyPos.at(1).first <= -WINDOW_WIDTH)
+        _skyPos.at(1).first = WINDOW_WIDTH;
+
+    float yOffset = 15.0f * std::sin(movement);
+
+    _graphical->drawSprite("sky", _skyPos.at(0).first, _skyPos.at(0).second + yOffset, 0, 0, 0.5, 0, WINDOW_WIDTH * 2, 245);
+    _graphical->drawSprite("sky", _skyPos.at(1).first, _skyPos.at(1).second + yOffset, 0, 0, 0.5, 0, WINDOW_WIDTH * 2, 245);
 }
