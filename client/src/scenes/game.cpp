b449340a43bf0f318dec18107d8a29d0c77109c3
Paul THIEBAULT <paul.thiebault@epitech.eu>
2024-01-13 18:39:23 +0100

    fix(client): fix parallax rotation

--- client/src/scenes/game.cpp
+++ client/src/scenes/game.cpp
@@ -15,7 +15,6 @@ void rtype::Client::drawGame()
     _graphical->clear();
     drawParallax();
     drawEntities();
-    // _graphical->drawSprite(_ecs.getComponent<Sprite>(player)->texture, _ecs.getComponent<Position>(_players[0])->x, _ecs.getComponent<Position>(_players[0])->y, 3, 180, 34, 34);
     _graphical->display();
 }
 
@@ -44,12 +43,11 @@ void rtype::Client::drawParallax()
         _parallaxPos.at(4).first = WINDOW_WIDTH;
     if (_parallaxPos.at(5).first <= -WINDOW_WIDTH)
         _parallaxPos.at(5).first = WINDOW_WIDTH;
-        
 
-    _graphical->drawSprite("parallax100", _parallaxPos.at(0).first, _parallaxPos.at(0).second, 0, 0, 1, 180, WINDOW_WIDTH, WINDOW_HEIGHT);
-    _graphical->drawSprite("parallax100", _parallaxPos.at(1).first, _parallaxPos.at(1).second, 0, 0, 1, 180, WINDOW_WIDTH, WINDOW_HEIGHT);
-    _graphical->drawSprite("parallax80", _parallaxPos.at(2).first, _parallaxPos.at(2).second, 0, 0, 1, 1, WINDOW_WIDTH, WINDOW_HEIGHT);
-    _graphical->drawSprite("parallax80", _parallaxPos.at(3).first, _parallaxPos.at(3).second, 0, 0, 1, 1, WINDOW_WIDTH, WINDOW_HEIGHT);
-    _graphical->drawSprite("parallax60", _parallaxPos.at(4).first, _parallaxPos.at(4).second, 0, 0, 1, 180, WINDOW_WIDTH, WINDOW_HEIGHT);
-    _graphical->drawSprite("parallax60", _parallaxPos.at(5).first, _parallaxPos.at(5).second, 0, 0, 1, 180, WINDOW_WIDTH, WINDOW_HEIGHT);
+    _graphical->drawSprite("parallax100", _parallaxPos.at(0).first, _parallaxPos.at(0).second, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    _graphical->drawSprite("parallax100", _parallaxPos.at(1).first, _parallaxPos.at(1).second, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    _graphical->drawSprite("parallax80", _parallaxPos.at(2).first, _parallaxPos.at(2).second, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    _graphical->drawSprite("parallax80", _parallaxPos.at(3).first, _parallaxPos.at(3).second, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    _graphical->drawSprite("parallax60", _parallaxPos.at(4).first, _parallaxPos.at(4).second, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    _graphical->drawSprite("parallax60", _parallaxPos.at(5).first, _parallaxPos.at(5).second, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 }
