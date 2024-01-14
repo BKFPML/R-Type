edb22743e902dbfa11cb9f11d813b371c855f139
Paul THIEBAULT <paul.thiebault@epitech.eu>
2024-01-14 17:04:00 +0100

    feat(server): Enemies Initialiser & load enemies texture

--- server/includes/enemies.hpp
+++ server/includes/enemies.hpp
@@ -14,22 +14,20 @@
  */
 struct Attributes {
     int health;
-    int rotation;
+    float rotation;
     std::string texture;
     int width, height, startX, startY;
     float scale;
 
-    Attributes(int health, int rotation, std::string texture, int width, int height, int startX, int startY, float scale) : health(health), texture(texture), width(width), height(height), startX(startX), startY(startY), scale(scale) {}
+    Attributes(int health, float rotation, std::string texture, int width, int height, int startX, int startY, float scale) : health(health), texture(texture), width(width), height(height), startX(startX), startY(startY), scale(scale) {}
     Attributes() : health(0), rotation(0), texture(""), width(0), height(0), startX(0), startY(0), scale(0.0f) {}
 };
 
 std::map<std::string, Attributes> createEnemyTypes()
 {
     std::map<std::string, Attributes> associations;
-    std::string crawler = "crawler";
-    std::string typesheet8 = "r_typesheet8";
-    associations[crawler] = Attributes(100, 180, typesheet8, 32, 32, 0, 0, 3);
-    // associations["robot"] = Attributes(100, 180, "r_typesheet9", 55, 55, 0, 0, 3);
+    associations["crawler"] = Attributes(100, 180, "r_typesheet8", 32, 32, 0, 0, 3);
+    associations["shooter"] = Attributes(200, 180, "r_typesheet9", 55, 55, 0, 0, 3);
 
     return associations;
 }
--- server/src/enemies.cpp
+++ server/src/enemies.cpp
@@ -11,13 +11,12 @@ void Server::init_enemies()
 {
     std::cout << "Init enemies" << std::endl;
     _enemies = _ecs.getEnemies();
+    std::map<std::string, Attributes> enemyTypes = createEnemyTypes();
     for (auto& enemy : _enemies) {
-        std::cout << "Enemy: " << _ecs.getComponent<Enemy>(enemy)->name << std::endl;
-        // _ecs.addComponent<Position>(enemy, Position(0, 0));
-        // _ecs.addComponent<Rotation>(enemy, Rotation(0));
-        // _ecs.addComponent<Velocity>(enemy, Velocity(0, 0));
-        // _ecs.addComponent<Health>(enemy, Health(100));
-        // _ecs.addComponent<Enemy>(enemy, Enemy());
-        // _ecs.addComponent<Sprite>(enemy, Sprite("assets/enemy.png"));
+        std::string enemyType = _ecs.getComponent<Enemy>(enemy)->name;
+        std::cout << "enemyType: " << enemyType << std::endl;
+        _ecs.addComponent<Health>(enemy, {enemyTypes[enemyType].health});
+        _ecs.addComponent<Rotation>(enemy, {enemyTypes[enemyType].rotation});
+        _ecs.addComponent<Sprite>(enemy, {enemyTypes[enemyType].texture, enemyTypes[enemyType].width, enemyTypes[enemyType].height, enemyTypes[enemyType].startX, enemyTypes[enemyType].startY, enemyTypes[enemyType].scale});
     }
 }
