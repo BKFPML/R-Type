fd513c172de95d325cd277e8605bebf37cb51020
Ben Desprets <benjy.desprets@gmail.com>
2024-01-14 13:41:27 +0100

    add(Enemies): enemies.hpp

---
+++ server/includes/enemies.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file enemies.hpp
+ * @brief 
+ * @date 14-01-2024
+ */
+
+#pragma once
+
+#include "../../engine/includes/ECS.hpp"
+#include "../../engine/includes/components.hpp"
+
+struct Attributes {
+    int health;
+    int rotation;
+    std::string texture;
+    int width, height, startX, startY;
+    float scale;
+
+    Attributes(int health, int rotation, std::string texture, int width, int height, int startX, int startY, float scale) : health(health), texture(texture), width(width), height(height), startX(startX), startY(startY), scale(scale) {}
+};
+
+std::map<std::string, Attributes> createEnemyTypes()
+{
+    std::map<std::string, Attributes> associations;
+    associations["crawler"] = Attributes(100, 180, "r_typesheet8", 32, 32, 0, 0, 3);
+    associations["robot"] = Attributes(100, 180, "r_typesheet9", 55, 55, 0, 0, 3);
+}
